28a0c6447e1717f0b76e20593b99f7aea3a8d57f
RyanSohaky <68452673+RyanSohaky@users.noreply.github.com>
2020-07-17 23:22:31 -0300

    Hack Naruto Online
    Não use

---
+++ Hacky.py
@@ -0,0 +1,35 @@
+from tkinter import Button, Entry, Label, Tk
+
+
+
+janela = Tk()
+
+
+janela.title('Lingotes De Graça')
+
+
+
+lb1 = Label(janela,text='login: ')
+titulo = Label(janela,text='Insira seu login e senha do Natuto Online ')
+lb2 = Label(janela,text='senha: ')
+
+titulo.config(font=("Courier", 20))
+
+ed1 = Entry(janela,)
+ed2 = Entry(janela,)
+
+bt1 = Button(janela, text='Confirmar')
+
+titulo.grid(row=0,column=1 )
+lb1.grid(row=1, column=0)
+lb2.grid(row=2, column=0)
+
+ed1.grid(row=1, column=1)
+ed2.grid(row=2, column=1)
+bt1.grid(row=3, column=1)
+#LxA+
+janela.geometry ()
+
+janela.mainloop()
+
+
